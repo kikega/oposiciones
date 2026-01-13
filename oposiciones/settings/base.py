2c2dacbbc28906b40c0bec975fc84eddd81d8a65
Enrique Gálvez <kikega@telefonica.net>
2026-01-13 23:19:58 +0100

    Configuracion del middleware del loging de usuarios en settings

--- oposiciones/settings/base.py
+++ oposiciones/settings/base.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "usuarios.middleware.access_logging.AccessLogMiddleware",
 ]
 
 ROOT_URLCONF = "oposiciones.urls"
